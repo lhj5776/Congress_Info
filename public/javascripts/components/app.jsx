783f66bc1e6c5974f905496f9c4e8c05fdf273e2
lhj5776 <lhj_5776@naver.com>
2017-02-17 19:33:58 +0900

    first

---
+++ public/javascripts/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Search from "./search";
+import Member from "./member";
+
+export default function App () {
+  return (
+    <Search />
+  );
+}
---
+++ public/javascripts/components/member.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Member extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    return (
+      <h1>Detail Information of Member</h1>
+    );
+  }
+}
